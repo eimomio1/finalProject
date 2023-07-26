5e6af6ebffdb6582a710607eb45965a3174a0baa
malbeastin <79538386+malbeastin@users.noreply.github.com>
2023-07-25 21:21:41 -0400

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-// login.js
-
-document.getElementById("loginForm").addEventListener("submit", function () {
-  var userTypeSelect = document.getElementById("userTypeSelect");
-  var userType = userTypeSelect.value;
-  document.getElementById("userType").value = userType;
-});
